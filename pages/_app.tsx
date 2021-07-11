b0977dd2ce9ed433451b6fc750461d2e873bf3d6
gagik <gagik@princeton.edu>
2021-07-11 01:27:19 -0400

    Add title.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,14 @@ import { UserProvider } from '@auth0/nextjs-auth0';
 import Layout from '../src/everpurple/Layout';
 import Everpurple from '../src/everpurple/Everpurple';
 import '../src/everpurple/style.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   return (
     <UserProvider>
+      <Head>
+        <title>Profile by Hoagie</title>
+      </Head>
       <Everpurple>
       <Layout>
       <Component {...pageProps} />
